728af606d0a1ce54ebb4b95e5fb1def51214576b
Benjamin Nørgaard <b@noergaard.dk>
2019-05-01 17:08:56 +0200

    Add retry logic

--- cmd/amznode/main.go
+++ cmd/amznode/main.go
@@ -3,16 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/blacksails/amznode"
 	"github.com/blacksails/amznode/pg"
 )
 
 func main() {
-	storage, err := pg.NewFromEnv()
-	if err != nil {
-		log.Fatal(err)
+	var (
+		storage amznode.Storage
+		err     error
+	)
+	ticker := time.NewTicker(time.Second)
+	tries := 0
+	for {
+		<-ticker.C
+		storage, err = pg.NewFromEnv()
+		if err == nil {
+			break
+		}
+		tries++
+		if tries > 10 {
+			log.Fatal(err)
+		}
 	}
+	ticker.Stop()
 	server := amznode.New(storage)
 	err = http.ListenAndServe(":8080", server.Handler())
 	if err != nil {
